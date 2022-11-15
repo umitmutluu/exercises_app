570eb08f3303d1b1be8c5bff0dee515a875cee24
umitmutluu <umitt.mmutlu@gmail.com>
2022-11-15 17:11:11 +0300

    Architect | Architect initialized

---
+++ lib/product/views/search_screen/viewmodel/cubit/search_screen_state.dart
@@ -0,0 +1,10 @@
+part of 'search_screen_cubit.dart';
+
+abstract class SearchScreenState extends Equatable {
+  const SearchScreenState();
+}
+
+class SearchScreenInitial extends SearchScreenState {
+  @override
+  List<Object> get props => [];
+}
