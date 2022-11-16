b76f6c9ea4ce76b8502ad1137cbca5d0b83b4feb
umitmutluu <umitt.mmutlu@gmail.com>
2022-11-16 15:55:36 +0300

    Cubit | cubit tools configured

--- lib/product/views/search_screen/viewmodel/cubit/search_screen_cubit.dart
+++ lib/product/views/search_screen/viewmodel/cubit/search_screen_cubit.dart
@@ -13,17 +13,21 @@ class SearchScreenCubit extends Cubit<SearchScreenState> {
 
   SearchScreenCubit(this.searchService) : super(SearchScreenInitialState());
 
-  late String muscle;
-  late String type;
+  late String valueKey;
 
   void changeLoading() {
     isPagingLoading = !isPagingLoading;
   }
 
+  void onValueChange(String value) {
+    valueKey = value;
+
+    fetchSearchItems();
+  }
+
   Future<void> fetchSearchItems() async {
     emit(const SearchScreenLoadingState(true));
-    final items =
-        await searchService.fetchSearchList(muscle: muscle, type: type);
+    final items = await searchService.fetchSearchList(value: valueKey);
     if (items == null) {
       emit(SearchScreenFailureState());
     } else {
